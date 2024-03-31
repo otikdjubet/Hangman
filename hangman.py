f071bfad431719cb24b4418c9cde804a81dd72ea
Jose Yivetto Dinampo <jyddinampo@gmail.com>
2024-03-31 06:17:56 -0700

    Second Commmit

--- hangman.py
+++ hangman.py
@@ -7,16 +7,17 @@ def get_valid_word(words):
     while '-' in word or ' ' in word:
         word = random.choice(words)
 
-    return word
+    return word.upper()
 
 def hangman():
     word = get_valid_word(words)
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
+    lives = 6
 
-    while len(word_letters) > 0:
-        print('You have used these letters: ', ' '.join(used_letters))
+    while len(word_letters) > 0 and lives > 0:
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print('Current word: ', ' '.join(word_list))
@@ -26,13 +27,22 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+                print('')
+            
+            else:
+                lives = lives - 1
+                print('Letter is not in the word.')
 
         elif user_letter in used_letters:
             print('You have already used that character. Please try again: ')
     
         else:
             print('Invalid character. Please try:')
+    if lives == 0:
+        print('You died. The word was', word)
+    else:
+        print('You guess the word', word, '!')
 
     
-
-user_input = input('type something:')
+if __name__ == '__main__':
+    hangman()
